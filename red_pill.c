b3c5bc1efd6d5b933c85c1dc390b5d3c7c6ae14e
arisumerray <alisashalamkova01@gmail.com>
2023-10-10 18:01:33 +0300

    Add red pill.c

---
+++ red_pill.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <cpuid.h>
+
+int main()
+{
+    unsigned int regs[12];
+    char str[sizeof(regs)+1];
+
+    __cpuid(0x80000000, regs[0], regs[1], regs[2], regs[3]);
+
+    if (regs[0] < 0x80000004)
+        return 1;
+
+    __cpuid(0x80000002, regs[0], regs[1], regs[2], regs[3]);
+    __cpuid(0x80000003, regs[4], regs[5], regs[6], regs[7]);
+    __cpuid(0x80000004, regs[8], regs[9], regs[10], regs[11]);
+
+    memcpy(str, regs, sizeof(regs));
+    str[sizeof(regs)] = '\0';
+    printf("%s\n", str);
+
+    return 0;
+}
+
