d8614005cb2d0796b85030ff2420bd8fb25c517f
MotaTasher <mot12131415@gmail.com>
2023-10-11 17:01:44 +0300

    Update names

--- include/Constants.h
+++ include/Constants.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-const std::string kCpuProcName = "cpuid_proc_name";
+const std::string kCpuidHvMan = "cpuid_hv_man";
 const std::string kCpuidMan = "cpuid_man";
-const std::string kCpuidHvMan = "hv_vendor_id";
+const std::string kCpuProcName = "cpuid_proc_name";
 
