5ee36cf2d3d2baa79da28656c74fd09ce43f1f64
MotaTasher <mot12131415@gmail.com>
2023-10-12 15:52:02 +0300

    Change arg name 'cpu_num_operation' -> 'cpu'

--- include/Constants.h
+++ include/Constants.h
@@ -5,4 +5,4 @@
 const std::string kCpuidHvMan       = "cpuid_hv_man";
 const std::string kCpuidMan         = "cpuid_man";
 const std::string kCpuProcName      = "cpuid_proc_name";
-const std::string kCpuNumOperation = "cpu_num_operation"; 
+const std::string kCpuNumOperation  = "cpu"; 
